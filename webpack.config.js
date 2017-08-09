b2fab4346785d6394a5af05003ce3803292aef98
Mathieu Pavageau <mth.pvg@gmail.com>
2017-08-09 21:41:48 +0200

    feat: enable webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+const commonConfig = {
   entry: {
     app: PATHS.app,
   },
@@ -19,4 +20,26 @@ module.exports = {
       title: 'Webpack demo',
     }),
   ],
+};
+
+const productionConfig = {};
+
+const developmentConfig = {
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. Good for complex setups.
+    historyApiFallback: true,
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+    host: '0.0.0.0',
+    port: '8080',
+  }
+};
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
+  } else {
+    return merge(commonConfig, developmentConfig);
+  }
 };
