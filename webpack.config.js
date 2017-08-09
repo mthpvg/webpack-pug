21e8c5209cfc42479bbb1e40bcd43eded6a4993c
Mathieu Pavageau <mth.pvg@gmail.com>
2017-08-09 21:52:39 +0200

    feat: add CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 
+const parts = require('./webpack.parts');
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-const commonConfig = {
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo',
-    }),
-  ],
-};
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
 
-const productionConfig = {};
+  },
+  parts.loadCSS(),
+]);
 
-const developmentConfig = {
-  devServer: {
-    // Enable history API fallback so HTML5 History API based
-    // routing works. Good for complex setups.
-    historyApiFallback: true,
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
     host: '0.0.0.0',
     port: '8080',
-  }
-};
+  }),
+]);
 
 module.exports = (env) => {
   if (env === 'production') {
---
+++ webpack.parts.js
@@ -0,0 +1,26 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    historyApiFallback: true,
+    stats: 'errors-only',
+    host,
+    port,
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
