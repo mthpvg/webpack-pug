9340de503d42ceb0b0d4359599884db411a5e2bc
Mathieu Pavageau <mth.pvg@gmail.com>
2018-04-27 08:41:24 +0200

    fix: npm run build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,16 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BabiliPlugin = require('babili-webpack-plugin');
+// TODO bring back js minimification: https://github.com/webpack-contrib/babel-minify-webpack-plugin/issues/68
+// const BabiliPlugin = require('babili-webpack-plugin');
 const OptimizeCSSAssetsPlugin =
   require('optimize-css-assets-webpack-plugin');
 const cssnano = require('cssnano');
 
-const extractTextPlugin = new ExtractTextPlugin({
-  filename: '[name].[contenthash:8].css',
-});
 
 module.exports = {
   mode: 'production',
-  devtool: 'source-map',
+  devtool: 'cheap-source-map',
   output: {
     chunkFilename: '[name].[chunkhash:8].js',
     filename: '[name].[chunkhash:8].js',
@@ -21,18 +19,20 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: extractTextPlugin.extract({
-          use: 'css-loader',
-          fallback: 'style-loader',
-        }),
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ],
       },
     ],
   },
   plugins: [
-    extractTextPlugin,
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:8].css"
+    }),
     new CleanWebpackPlugin([path.join(__dirname, 'build')]),
     // Minify Javascript
-    new BabiliPlugin(),
+    // new BabiliPlugin(),
     // Minify CSS
     new OptimizeCSSAssetsPlugin({
       cssProcessor: cssnano,
