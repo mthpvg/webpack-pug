65c54b7ee723bb45e38fb57fdefdda3831d00f66
Mathieu Pavageau <mth.pvg@gmail.com>
2018-08-15 21:25:48 +0200

    chore: remove second page as we build a SPA

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const pages = buildPages();
-
 
 module.exports = {
   entry: {
     index: path.join(__dirname, 'app/pages/index/js/index.js'),
-    secondary: path.join(__dirname, 'app/pages/secondary/js/index.js'),
     vendor: ['react', 'react-dom']
   },
   output: {
@@ -23,15 +20,14 @@ module.exports = {
           loader: "babel-loader"
         }
       },
-        { test: /\.pug$/, loader: "pug-loader" },
-        // { test: /\.json$/, loader: "json-loader" },
-        {
-          test: /\.(jpg|jpeg|png|svg)$/,
-          loader: 'url-loader',
-          options: {
-            name: '[name].[hash:8].[ext]',
-          },
+      { test: /\.pug$/, loader: "pug-loader" },
+      {
+        test: /\.(jpg|jpeg|png|svg)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[hash:8].[ext]',
         },
+      }
     ]
   },
   plugins: [
@@ -41,7 +37,7 @@ module.exports = {
       inject: 'body',
       chunks: ['index', 'vendor']
     }),
-  ].concat(pages),
+  ],
   optimization: {
     splitChunks: {
       cacheGroups: {
@@ -54,18 +50,4 @@ module.exports = {
       }
     }
   }
-};
-
-function buildPages() {
-  const config = require('./app/pages/config.json');
-  return config.map(function (page) {
-    return new HtmlWebpackPlugin({
-      template: path.join(
-        __dirname, 'app/pages/' + page.name+ '/view/index.pug'
-      ),
-      filename: page.url + '/index.html',
-      inject: 'body',
-      chunks: [page.name]
-    })
-  });
-};
+}
